684adc0cf77c0eb648398132f963e6711cf5952a
ZSY-desktop <heihiha@outlook.com>
2024-03-28 17:30:46 +0800

    add functions to nvim-ufo

--- lua/plugins/coding/nvim-ufo.lua
+++ lua/plugins/coding/nvim-ufo.lua
@@ -1,10 +1,38 @@
 -- PLUGIN: nvim-ufo
 -- FUNCTIONALITY: folding and unfolding code blocks
 vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 9 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 9
-vim.o.foldenable = false -- show fold level
--- vim.o.foldenable = true -- show fold level
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+-- vim.o.foldenable = false -- show fold level
+vim.o.foldenable = true -- show fold level
+
+local handler = function(virtText, lnum, endLnum, width, truncate)
+	local newVirtText = {}
+	local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+	local sufWidth = vim.fn.strdisplaywidth(suffix)
+	local targetWidth = width - sufWidth
+	local curWidth = 0
+	for _, chunk in ipairs(virtText) do
+		local chunkText = chunk[1]
+		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+		if targetWidth > curWidth + chunkWidth then
+			table.insert(newVirtText, chunk)
+		else
+			chunkText = truncate(chunkText, targetWidth - curWidth)
+			local hlGroup = chunk[2]
+			table.insert(newVirtText, { chunkText, hlGroup })
+			chunkWidth = vim.fn.strdisplaywidth(chunkText)
+			-- str width returned from truncate() may less than 2nd argument, need padding
+			if curWidth + chunkWidth < targetWidth then
+				suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+			end
+			break
+		end
+		curWidth = curWidth + chunkWidth
+	end
+	table.insert(newVirtText, { suffix, "MoreMsg" })
+	return newVirtText
+end
 
 return {
 	"kevinhwang91/nvim-ufo",
@@ -18,6 +46,20 @@ return {
 	},
 	config = function()
 		require("ufo").setup({
+			fold_virt_text_handler = handler,
+			preview = {
+				win_config = {
+					border = { "", "─", "", "", "", "─", "", "" },
+					winhighlight = "Normal:Folded",
+					winblend = 0,
+				},
+				mappings = {
+					scrollU = "<C-u>",
+					scrollD = "<C-d>",
+					jumpTop = "[",
+					jumpBot = "]",
+				},
+			},
 			provider_selector = function()
 				return { "treesitter", "indent" }
 			end,
