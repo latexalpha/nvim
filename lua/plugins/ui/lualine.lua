100e1a9bf5f60bdbc3e4f54d53a37bfe7b456c99
ZSY-desktop <heihiha@outlook.com>
2024-03-28 16:47:45 +0800

    change ui setting with cmd view and cmdline position

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,12 +1,22 @@
 -- Package: lualine.nvim
 -- FUNCTIONALITY: statusline
 return {
-    "nvim-lualine/lualine.nvim",
-    event = "VimEnter",
-    dependencies = {
-    	"nvim-web-devicons",
-    },
-    config = function()
-	    require("lualine").setup()
-    end,
+	"nvim-lualine/lualine.nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-web-devicons",
+	},
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		})
+	end,
 }
--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,36 +1,55 @@
 -- PLUGIN: noice.nvim
 -- FUNCTIONALITY: noice ui
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
+	"folke/noice.nvim",
+	event = "VeryLazy",
 
-    dependencies = {
-        {
-            "MunifTanjim/nui.nvim",
-            lazy = true,
-        },
-        {
-            "rcarriga/nvim-notify",
-            config = function ()
-                require("notify").setup({
-                    background_colour = "#1e1e2e",
-                })
-            end
-        },
-    },
+	dependencies = {
+		{
+			"MunifTanjim/nui.nvim",
+			lazy = true,
+		},
+		{
+			"rcarriga/nvim-notify",
+			config = function()
+				require("notify").setup({
+					background_colour = "#1e1e2e",
+				})
+			end,
+		},
+		{
+			"hrsh7th/nvim-cmp",
+		},
+	},
 
-    opts = {
-        lsp = {
-            override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-            },
-        },
-        presets = {
-            bottom_search = true,
-            command_palette = true,
-            long_message_to_split = true,
-        },
-    },
-
+	opts = {
+		lsp = {
+			progress = { enabled = false },
+			-- signature = { enabled = false },
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
+		presets = {
+			bottom_search = false, -- use a classic bottom cmdline for search
+			command_palette = false,
+			long_message_to_split = true,
+		},
+		views = {
+			cmdline_popup = {
+				-- border = {
+				style = "none",
+				-- style = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+				padding = { 1, 3 },
+				-- },
+				-- win_options = { winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder" },
+				position = { row = 20, col = "50%" },
+			},
+		},
+	},
+	config = function(_, opts)
+		require("noice").setup(opts)
+	end,
 }
