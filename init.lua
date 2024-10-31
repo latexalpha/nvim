bd9dfab8e969e4ecbfc89211820904930b0dd634
ZSY-desktop <latexalpha@outlook.com>
2024-10-31 22:10:43 +0800

    change global python for linux

--- init.lua
+++ init.lua
@@ -78,7 +78,7 @@ if binaryformat == "dll" then
 	g.vimtex_view_general_viewer = "SumatraPDF"
 	g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
 elseif binaryformat == "so" then
-	g.python3_host_prog = "~/miniconda3/bin/python" -- set the Nvim python virtual environment
+	g.python3_host_prog = "/usr/bin/python3" -- set the Nvim python virtual environment
 	vim.cmd([[
         if has("nvim") && !empty($CONDA_PREFIX)
             let g:python3_host_prog = $CONDA_PREFIX . "/bin/python"
